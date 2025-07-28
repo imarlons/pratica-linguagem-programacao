f4b4045cce47a0a577b252c5186b5b4a47181b8a
imarlons <marlon.silva@aluno.fmpsc.edu.br>
2025-07-28 10:16:19 -0300

    ajustes

--- Aula 01/index.php
+++ Aula 01/index.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
-    <meta charset="ISO-8859-1">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Teste PHP</title>
+    <title>Aula 01</title>
 </head>
 
 <body>
