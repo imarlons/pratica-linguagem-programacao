230b9ecea13732336787ff292710f8331d2229f9
imarlons <marlon.silva@aluno.fmpsc.edu.br>
2025-07-28 10:07:17 -0300

    primeiro teste com php e xampp

---
+++ Aula 03/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="ISO-8859-1">
+    <link rel="stylesheet" href="style.css">
+    <title>Teste PHP</title>
+</head>
+
+<body>
+    <header>
+        <h1>Faculdade Municipal de Palhoça</h1>
+    </header>
+    <main>
+        <p>Bem-vindo à Faculdade Municipal de Palhoça!</p>
+        <p>Curso: Análise e Desenvolvimento de Sistemas.</p>
+        <p>Disciplina: Prática de Linguagem de Programação I.</p>
+        <p>Aluno: Marlon da Silva</p>
+    </main>
+    <!-- From Uiverse.io by BlackisPlay -->
+    <div id="ghost">
+        <div id="red">
+            <div id="pupil"></div>
+            <div id="pupil1"></div>
+            <div id="eye"></div>
+            <div id="eye1"></div>
+            <div id="top0"></div>
+            <div id="top1"></div>
+            <div id="top2"></div>
+            <div id="top3"></div>
+            <div id="top4"></div>
+            <div id="st0"></div>
+            <div id="st1"></div>
+            <div id="st2"></div>
+            <div id="st3"></div>
+            <div id="st4"></div>
+            <div id="st5"></div>
+            <div id="an1"></div>
+            <div id="an2"></div>
+            <div id="an3"></div>
+            <div id="an4"></div>
+            <div id="an5"></div>
+            <div id="an6"></div>
+            <div id="an7"></div>
+            <div id="an8"></div>
+            <div id="an9"></div>
+            <div id="an10"></div>
+            <div id="an11"></div>
+            <div id="an12"></div>
+            <div id="an13"></div>
+            <div id="an14"></div>
+            <div id="an15"></div>
+            <div id="an16"></div>
+            <div id="an17"></div>
+            <div id="an18"></div>
+        </div>
+        <div id="shadow"></div>
+    </div>
+
+    <footer>
+        <p>&copy; 2025 Faculdade Municipal de Palhoça</p>
+    </footer>
+</body>
+
+</html>
