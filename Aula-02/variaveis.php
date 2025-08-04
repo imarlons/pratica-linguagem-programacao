1a6c64eae7b0f576a7b0ed4613369253d489207d
imarlons <marlon.silva@aluno.fmpsc.edu.br>
2025-08-04 08:39:20 -0300

    introdução a variaveis com php

---
+++ Aula-02/variaveis.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 02</title>
+</head>
+
+<body>
+    <?php
+    $nome = 'marlon da silva';
+    $idade = 22;
+    // ponto (.) serve para concatenar
+    echo 'meu nome é ' . $nome . ' e tenho ' . $idade . ' anos!'
+    ?>
+</body>
+
+</html>
