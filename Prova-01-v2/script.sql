a5c11b39239ba858ac7bcec936439338b7309c69
imarlons <marlon.silva@aluno.fmpsc.edu.br>
2025-11-06 20:23:42 -0300

    feat: add script sql

--- Prova-01-v2/script.sql
+++ Prova-01-v2/script.sql
@@ -1,16 +1,15 @@
 -- garante a criação do banco de dados apenas se ele não existir
-CREATE DATABASE IF NOT EXISTS `cadastro_produtos` 
-DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE IF NOT EXISTS cadastro_produtos DEFAULT CHARACTER SET utf8mb4;
 
 -- seleciona o banco de dados recém-criado para os comandos seguintes
 USE `cadastro_produtos`;
 
 -- cria a tabela 'produtos' se ela não existir
-CREATE TABLE IF NOT EXISTS `produtos` (
-  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `nome_produto` VARCHAR(255) NOT NULL,
-  `descricao` TEXT DEFAULT NULL,
-  `preco` DECIMAL(10,2) NOT NULL,
-  `categoria` VARCHAR(100) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE IF NOT EXISTS produtos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome_produto VARCHAR(255) NOT NULL UNIQUE, -- o nome é a chave para exclusão/atualização
+    descricao TEXT,
+    preco DECIMAL(10, 2) NOT NULL,
+    categoria VARCHAR(100),
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
