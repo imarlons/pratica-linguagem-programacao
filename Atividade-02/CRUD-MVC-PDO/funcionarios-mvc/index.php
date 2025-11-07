e6901ae8cc0b47bfa4cc8963475eaee758de86ea
imarlons <marlon.silva@aluno.fmpsc.edu.br>
2025-11-07 00:00:41 -0300

    feat: add develop index

---
+++ Atividade-02/CRUD-MVC-PDO/funcionarios-mvc/index.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once 'controller/FuncionarioController.php';
+
+$controller = new FuncionarioController();
+
+// define a ação com base no parâmetro 'action' da URL
+$action = $_GET['action'] ?? 'listar'; // ação padrão é 'listar'
+
+switch ($action) {
+    case 'listar':
+        $controller->listar();
+        break;
+    case 'criar':
+        $controller->criar();
+        break;
+    case 'salvar':
+        $controller->salvar();
+        break;
+    case 'editar':
+        $controller->editar();
+        break;
+    case 'atualizar':
+        // $controller->atualizar(); // Implementação futura
+        break;
+    case 'excluir':
+        $controller->excluir();
+        break;
+    default:
+        $controller->listar();
+        break;
+}
